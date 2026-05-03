81013c57d44dc67a2d47e1005063dc779c3925a0
Minji <wjdalswl6756@naver.com>
2026-05-04 04:04:59 +0900

    main 최적화

--- offline_main.py
+++ offline_main.py
@@ -1,4 +1,4 @@
-import numpy as np
+"""import numpy as np
 import config 
 import time 
 import pickle
@@ -149,6 +149,233 @@ def run_pipeline(data_path, paper_batch_size):
 
     # 3. 데이터셋 순회하며 파이프라인 실행 (paper_batch_size(예: 32) 단위로 쪼갬)
     for i in range(0, total_papers, paper_batch_size):
+        paper_batch = eval_data[i : i + paper_batch_size]
+        # 논문 100개, batch : 32일때 마지막 루프 i=96일땐 96~128(96+32)가 아닌 96~100이어야하므로 min 취함 
+        print(f"처리 중 ... 논문 [{i} ~ {min(i + paper_batch_size, total_papers)}] / {total_papers}")
+
+        batch_results = process_paper_batch(paper_batch, query_builder, embedder, retriever, bib_scorer, embedding_db)
+        
+        # 배치 단위 성능 평가 로직 
+        batch_queries_count = len(batch_results)
+        if batch_queries_count > 0:
+            batch_metrics = {"Recall@50": 0.0, "Recall@100": 0.0, "Recall@150":0.0, "MRR": 0.0}
+
+            for q_data in batch_results:
+                predicted_ids = [cand['paper_id'] for cand in q_data['candidates']]
+                gt_ids = q_data['target_ids']
+
+                
+                # 쿼리당 채점 
+                metrics = calculate_metrics(predicted_ids, gt_ids)
+
+
+                # 배치 및 global metrics에 누적 
+                for key in global_metrics:
+                    batch_metrics[key] += metrics[key]
+                    global_metrics[key] += metrics[key]
+            
+            total_queries_so_far += batch_queries_count
+
+            # 배치 평균 성능 출력
+            print(f"[Batch 성능] Recall@50: {batch_metrics['Recall@50'] / batch_queries_count:.4f} | Recall@100: {batch_metrics['Recall@100'] / batch_queries_count:.4f} | Recall@150: {batch_metrics['Recall@150'] / batch_queries_count:.4f} | MRR: {batch_metrics['MRR'] / batch_queries_count:.4f}")
+        
+        all_processed_queries.extend(batch_results)# 다음 파트에 합치기 (batch_results 이용할지 말지)
+    
+    # 모든 배치가 끝난 후 최종 전체 성능 평가 결과 출력
+    if total_queries_so_far > 0:
+        print("\n" + "="*45)
+        print(f"최종 전체 성능 (Total Queries: {total_queries_so_far}개)")
+        print("="*45)
+        for key in global_metrics:
+            final_avg = global_metrics[key] / total_queries_so_far
+            print(f" - {key}: {final_avg:.4f}")
+        print("="*45 + "\n")
+   
+    print(f"총 소요시간 : {time.time() - start_time: .2f}초")
+
+    return all_processed_queries
+
+    
+
+if __name__ == "__main__":
+    final_data = run_pipeline(config.EVAL_DATA_PATH, config.PAPER_BATCH_SIZE)
+    utils.save_json(final_data, "offline_output.json") 
+    print("'offline_output.json' 저장 완료")
+"""
+
+import numpy as np
+import config 
+import time 
+import pickle
+import utils
+from tqdm import tqdm 
+from query_builder import QueryBuilder
+from embedder import SpecterEmbedder
+from retriever import FaissRetriever
+from soft_bias import SoftBiasScorer
+from fusion_var import rank_fusion_var
+from evaluate import calculate_metrics
+
+
+def process_paper_batch(paper_batch, query_builder, embedder, retriever, bib_scorer, embedding_db, paper_query_top_k = config.PAPER_QUERY_TOP_K):
+    # paper_batch : eval_data에서 32개 논문 가져온 리스트 (json 형태)
+    final_output_for_next = []
+    
+    # 논문 단위로 순회 
+
+    for item in paper_batch:
+        paper_id = item.get('paper_id', '')
+
+        # QueryBuilder를 통해 paper query 1개, context query N개 추출 
+        paper_query, context_queries = query_builder.build_offline_query(
+            paper_id, item.get('full_text',''), item.get('title', ''), item.get('abstract',''), item.get('all_references', [])
+        )
+
+        # DB에 있는 논문만 GT로 구성, GT가 유효한 문맥만 필터링 
+        valid_contexts = []
+        # 해당 논문의 모든 인용구([CITE:])를 context_query_list에 저장
+        for sample in context_queries:
+            # [for 초기 데이터] db에 존재하는 진짜 정답만 추려냄 
+            vt = [tid for tid in sample['target_ids'] if tid in embedding_db]
+            if vt:
+                sample['target_ids'] = vt
+                valid_contexts.append(sample)
+
+        if not valid_contexts: continue 
+
+        # 2. 오프라인 필터링 (논문당 1번만 실행)
+        # title [SEP] abstract (query_builder에서 처리)
+        p_vec = embedder.encode([paper_query])
+        p_res = retriever.search(p_vec, [paper_id], top_k = config.PAPER_QUERY_TOP_K)[0]
+
+        # 후보 5000개의 벡터를 DB에서 한꺼번에 추출 
+        p_ids = [res['paper_id'] for res in p_res]
+
+        # 이때, embedding_db에 존재하는 논문만 가져와 (i, 벡터)을 담은 리스트 생성 
+        # p_ids = [A,B,C,D], embedding db에 A,C만 있어도,
+        # valid_data = [(0, vecA), (2,vecC)]로 저장됨 
+        
+        valid_data = [(i, embedding_db[pid]) for i,pid in enumerate(p_ids) if pid in embedding_db]
+        if not valid_data: 
+            continue
+        
+        # (1,2,3,...) 
+        # ([0.1, 0.2, ...], [0.4, 0.5, ...])
+        v_indices, t_vectors = zip(*valid_data)
+
+        # 벡터 -> 행렬 변환 
+        target_matrix = np.array(t_vectors).squeeze() # Shape: (5000, 768)
+        
+        # v_indices에 있는 index 기준으로 p_res에서 score 가져오기 
+        valid_p_sims = np.array([p_res[i]['score'] for i in v_indices])
+        # 해당 논문 paper id 가져옴 
+        valid_p_ids = [p_ids[i] for i in v_indices]
+
+        # 3. 행렬 연산으로 모든 문맥 한꺼번에 계산 
+        # [ "deep learning for NLP", "transformer architecture paper","attention mechanism explanation"]
+        c_queires = [ctx['context_query'] for ctx in valid_contexts]
+        c_vecs = embedder.encode(c_queires) # 문맥들 배치 인코딩 [[0.12, ...], [1,2,...]]
+
+        # 모든 문맥에 대해 한꺼번에 유사도 계산
+        # (문맥개수, 5000)
+        '''
+        c_sims_all =
+        [
+          [0.91, 0.12, ..., 0.33],   # 문맥1 vs 모든 논문
+          [0.44, 0.88, ..., 0.22],   # 문맥2 vs 모든 논문
+          ...
+        ]
+        '''
+        c_sims_all = np.dot(c_vecs, target_matrix.T)
+
+        # 4. 문맥별로 최종 순위 계산 및 패키징 
+        for i, sample in enumerate(valid_contexts):
+            c_sims = c_sims_all[i]
+            # 4-1. paper 점수 0~1 정규화 
+            p_min, p_max = np.min(valid_p_sims), np.max(valid_p_sims)
+            p_norm = (valid_p_sims - p_min) / (p_max - p_min + 1e-8) # 작은 수 더해서 0으로 나누는 경우 방지 
+
+
+            # 4-2. context 점수 0~1 정규화 
+            c_min, c_max = np.min(c_sims), np.max(c_sims)
+            c_norm = (c_sims - c_min) / (c_max - c_min + 1e-8) # 반환값 shape: (3000,)
+
+            # 4-3. 가중합도 NumPy로 한 번에 처리
+            final_sims = (config.PAPER_SIM_WEIGHT * p_norm) + (config.CONTEXT_SIM_WEIGHT * c_norm)
+
+            # 4-4. top-k 정렬 
+            top_idx = np.argsort(final_sims)[::-1][:config.TOP_K_FINAL]
+
+            candidates = []
+            
+            for rank, idx in enumerate(top_idx):
+                candidates.append({
+                    "paper_id": valid_p_ids[idx],
+                    "sim": float(final_sims[idx])
+                })
+
+            # Soft Bias
+            raw_bibs = sample.get('bib_ids', [])
+            valid_user_bibs = [b for b in raw_bibs if b in embedding_db]
+            biased = bib_scorer.soft_bias(candidates, valid_user_bibs, embedding_db)
+            
+            # 최종 피처 정리
+            norm_sims = np.array([c['sim'] for c in biased])
+            raw_scores = np.array([c.get('bib_score', 0.0) for c in biased])
+            b_min, b_max = np.min(raw_scores), np.max(raw_scores)
+            norm_bibs = (raw_scores - b_min) / (b_max - b_min + 1e-9) if b_max > b_min else np.zeros_like(raw_scores)
+
+            clean_candidates = [{
+                "paper_id": cand['paper_id'],
+                "sim": float(norm_sims[idx]),
+                "bib_score": float(norm_bibs[idx])
+            } for idx, cand in enumerate(biased)]
+
+            final_output_for_next.append({
+                "query_id": sample['query_id'],
+                "target_ids": sample['target_ids'],
+                "context": sample['context_query'],
+                "candidates": clean_candidates
+            })
+
+    return final_output_for_next
+
+
+        
+
+
+
+
+
+def run_pipeline(data_path, paper_batch_size):
+    '''
+    [동작 방식] 전체 데이터셋을 논문 단위로 쪼개고, 논문 내에서도 context 단위로 쪼개어 동작
+    '''
+    print(f"[Offline 실험용 추천 파이프라인 가동 시작...] (데이터: {data_path}")
+    start_time = time.time()
+
+    # 1. 모듈 생성 
+    query_builder = QueryBuilder()
+    embedder = SpecterEmbedder()
+    retriever = FaissRetriever()
+    bib_scorer = SoftBiasScorer()
+
+    # 2. 데이터셋 로드 (정답지 포함된 JSON 파일)
+    eval_data = utils.load_json(data_path)
+    with open(config.EMBEDDING_DB_PATH, "rb") as f:
+        embedding_db = pickle.load(f)
+
+    total_papers = len(eval_data)
+    all_processed_queries = [] # 모든 배치를 1차원으로 통합할 리스트 (할지말지 고민)
+
+    print(f"총 논문 개수 : {total_papers}개 (논문 {paper_batch_size}개씩 묶어서 처리)")
+
+    # 전체 데이터 global metrics 누적 변수 초기화 
+    total_queries_so_far = 0
+    global_metrics = {"Recall@50": 0.0, "Recall@100": 0.0, "Recall@150": 0.0, "MRR": 0.0}
+
+    # 3. 데이터셋 순회하며 파이프라인 실행 (paper_batch_size(예: 32) 단위로 쪼갬)
+    for i in tqdm(range(0, total_papers, paper_batch_size), desc = "배치 처리중"):
         paper_batch = eval_data[i : i + paper_batch_size]
         # 논문 100개, batch : 32일때 마지막 루프 i=96일땐 96~128(96+32)가 아닌 96~100이어야하므로 min 취함 
         print(f"처리 중 ... 논문 [{i} ~ {min(i + paper_batch_size, total_papers)}] / {total_papers}")
