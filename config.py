837c9ef4d8efa8175fecce1ac90e3f5926114399
Minji <wjdalswl6756@naver.com>
2026-05-11 14:13:17 +0900

    수정

--- config.py
+++ config.py
@@ -37,7 +37,7 @@ CITE_TAG_PATTERN = r"\[CITE:(.*?)\]"
 
 # 3. Retrieval & Fusion 하이퍼파라미터 설정
 NUM_SENTENCES = 4           # Context Query 생성시 placeholder 기준 자를 문장 수 
-SIMILARITY_THRESHOLD = 0.6  # FAISS 코사인 유사도 최소 임계값
+SIMILARITY_THRESHOLD = 0.65  # FAISS 코사인 유사도 최소 임계값
 FULL_TOPK = 5000    # paper query 기준 추려올 후보 논문 개수 
 TITLE_TOPK = 1000
 ABSTRACT_TOPK = 1000
