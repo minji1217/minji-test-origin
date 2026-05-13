899ae722ffb5a7e48e53498fe09513d966047cd4
Minji <wjdalswl6756@naver.com>
2026-05-14 08:55:44 +0900

    tw

--- config.py
+++ config.py
@@ -41,11 +41,11 @@ SIMILARITY_THRESHOLD = 0.0  # FAISS 코사인 유사도 최소 임계값
 FULL_TOPK = 2000    # paper query 기준 추려올 후보 논문 개수 
 BIB_CANDIDATE_SIZE = 1500
 
-BIB_WEIGHT = 0.06
+BIB_WEIGHT = 0.07
 TOP_K_FINAL = 150           # 후보 논문들 중 context query 통해 가져올 후보 개수 
 # RRF_K = 60                # RRF 스무딩 상수 
 PAPER_BATCH_SIZE = 256      # 논문 배치 크기 (for main)
 QUERY_BATCH_SIZE = 256      # 쿼리 배치 크기 (for encode) 
 MAX_SEQ_LENGTH = 512        # SPECTER2 최대 입력 크기 
-PAPER_SIM_WEIGHT = 0.3    # 가중합 비율 (paper_query) 
-CONTEXT_SIM_WEIGHT = 0.7    # 가중합 비율 (context_query)
+PAPER_SIM_WEIGHT = 0.4    # 가중합 비율 (paper_query) 
+CONTEXT_SIM_WEIGHT = 0.6    # 가중합 비율 (context_query)
